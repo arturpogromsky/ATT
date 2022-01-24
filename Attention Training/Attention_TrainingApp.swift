96677afe47ad5bad899cf8649ac170b6f4d7db21
arturpogromsky <ar.pogromsky@gmail.com>
2022-01-24 10:41:42 +0300

    Initial Commit

---
+++ Attention Training/Attention_TrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Attention_TrainingApp.swift
+//  Attention Training
+//
+//  Created by Артур Погромский on 24.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Attention_TrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
